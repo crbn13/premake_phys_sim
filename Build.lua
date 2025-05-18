5fbf3b2219a2fbb870c39e6b58fce47d1e972c42
rory <roryredking8@gmail.com>
2025-05-18 23:38:05 +0100

    somem customisations untested

--- Build.lua
+++ Build.lua
@@ -1,5 +1,5 @@
 -- premake5.lua
-workspace "New Project"
+workspace "phys_sim"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
    startproject "App"
@@ -8,10 +8,10 @@ workspace "New Project"
    filter "system:windows"
       buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
 
-OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+OutputDir = "{cfg.buildcfg}"
 
 group "Core"
 	include "Core/Build-Core.lua"
-group ""
+group "App"
 
 include "App/Build-App.lua"
